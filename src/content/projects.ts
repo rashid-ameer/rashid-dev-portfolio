3e4b9ab3b20470b9a09196d8da5e39f837fa2862
Rashid Ameer <r.ameer.offical@gmail.com>
2025-10-10 10:59:08 +0500

    Fix projects

---
+++ src/content/projects.ts
@@ -0,0 +1,45 @@
+export const PROJECTS = [
+  {
+    title: "Wild Travel",
+    description:
+      "A travel landing page inspired by adventure and nature. It highlights destinations, trip details, and features like offline maps, augmented reality guidance, and adventure scheduling — all in a modern, minimal layout.",
+    image: "/realtime-note-taking-web-app-ui-clean-minimal-prev.jpg",
+    tags: ["Next.js", "TypeScript", "Tailwind CSS"],
+    href: "https://wild-travel.vercel.app/",
+    repo: "#",
+  },
+  {
+    title: "DigiXtra",
+    description:
+      "A simple landing page for a software company presenting its services, portfolio, and contact information in a structured, easy-to-navigate layout.",
+    image: "/stripe-subscriptions-dashboard-minimal-dark.jpg",
+    tags: ["Next.js", "TypeScript", "Tailwind CSS"],
+    href: "https://digixtra.vercel.app/",
+    repo: "#",
+  },
+  {
+    title: "Ableton",
+    description:
+      "A clean landing page inspired by Ableton’s official website, showcasing its music production tools, creative workflow, and product highlights with a modern layout.",
+    image: "/stripe-subscriptions-dashboard-minimal-dark.jpg",
+    tags: ["HTML", "CSS", "JavaScript"],
+    href: "https://vercel.com/raofficals-projects/ableton",
+    repo: "#",
+  },
+  {
+    title: "Mercel",
+    description:
+      "A minimal social app for sharing posts, comments, and likes with a clean, responsive UI.",
+    image: "/stripe-subscriptions-dashboard-minimal-dark.jpg",
+    tags: [
+      "Next.js",
+      "TypeScript",
+      "React Query",
+      "Tailwind CSS",
+      "Shadcn UI",
+      "Prisma",
+    ],
+    href: "https://mercel-beta.vercel.app/",
+    repo: "#",
+  },
+];
